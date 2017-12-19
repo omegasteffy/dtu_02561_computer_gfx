904a5d13488ed97d494e2366dbd856c35780b43f
Steffen Villadsen <omegasteffy@gmail.com>
2017-12-19 21:57:37 +0100

    got my checkerboard

--- work/ws6/ws6_part1.js
+++ work/ws6/ws6_part1.js
@@ -20,9 +20,15 @@ var quadSpec;
 var gl;
 var program;
 var uniforms;
+var myTexel = new Image();
+myTexel.src = "https://webglfundamentals.org/webgl/resources/f-texture.png"
+//myTexel.addEventListener('load',continue_stuff);
 
+//function continue_stuff(){
+	setup_stuff();
+//}
 function setup_stuff() {
-	console.trace("Started");
+	console.trace("Started");	
 	var canvas = document.getElementById('draw_area');
 
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -69,56 +75,81 @@ function setup_stuff() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-	var vertice_colors = new Float32Array(
+	const vertice_colors = new Float32Array(
 		[1.0, 0.0, 0.0, 1.0, //Red
 			0.0, 1.0, 0.0, 1.0, //Green
 			0.0, 0.0, 1.0, 1.0, //Blue
 			0.0, 1.0, 1.0, 1.0] //Blue
 	);
 
+	//this layout is not accordingly to the book
+	const texCoords = [
+		vec2(0,0),
+		vec2(1,0),
+		vec2(0,1),
+		vec2(1,1)
+	];
+
 	quadSpec = quad(gl);
 
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	
 
-	let coordinates = coordinateSystem(gl);
-	send_array_to_attribute_buffer("a_Position", coordinates.points, 3, gl, program);
-	send_array_to_attribute_buffer("a_Color", coordinates.colors, 4, gl, program);
-	gl.drawArrays(coordinates.drawtype, 0, coordinates.drawCount);
+	// let coordinates = coordinateSystem(gl);
+	// send_array_to_attribute_buffer("a_Position", coordinates.points, 3, gl, program);
+	// send_array_to_attribute_buffer("a_Color", coordinates.colors, 4, gl, program);
+	// gl.drawArrays(coordinates.drawtype, 0, coordinates.drawCount);
 
 	let texSize = 64;
 	let numRows = 8;
 	let numCols = 8;
 	let index = 0;
 	let image_checker = new Uint8Array(4 /*RGBA*/ * texSize * texSize);
-	for (let i = 0; i < texSize; ++i ) // why not i++; ???
+	for (let i = 0; i < texSize; i++ ) // why not i++; ???
 	{
-		for (let j = 0; j < texSize; ++j)
+		for (let j = 0; j < texSize; j++)
 		{
-			const patchY = Math.floor(i / (texSize / numRows));
-			const patchX = Math.floor(j / (texSize / numCols));
+			const patchY = Math.floor(i / (texSize / numRows) );
+			const patchX = Math.floor(j / (texSize / numCols) );
 			const is_white = (patchX % 2 !== patchY % 2);
-			const color_intensity = is_white ? 255 : 0;
+			const color_intensity = is_white ? 255: 0;
 			
-			image_checker[index++] = color_intensity;
-			image_checker[index++] = color_intensity;
-			image_checker[index++] = color_intensity;
-			image_checker[index++] = 255; //alpha value
+			// image_checker[index++] = color_intensity;
+			// image_checker[index++] = color_intensity;
+			// image_checker[index++] = color_intensity;
+			// image_checker[index++] = 255; //alpha value
+
+			const indexBase =4*i *texSize+4*j;
+			 image_checker[indexBase] = color_intensity;
+			 image_checker[indexBase+1] = color_intensity;
+			 image_checker[indexBase+2] = color_intensity;
+			 image_checker[indexBase+3] = 255; //alpha value
 		}
 	}
 
-	//let myTexel = new Image();
-	//myTexel.src = "logo.gif"
-	//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	//gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, myTexel);
+
+	let texture = gl.createTexture();
+	
+	
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 64, 64, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+		image_checker,0);
+
+	gl.generateMipmap(gl.TEXTURE_2D); //do not work if not enabled
 
 	send_floats_to_attribute_buffer("a_Position", quadSpec.vertices, 3, gl, program);
 	send_floats_to_attribute_buffer("a_Color", vertice_colors, 4, gl, program);
+	const flat = flatten(texCoords);
+	send_floats_to_attribute_buffer("a_texCoordinate",flat, 2, gl, program);
+	var textureLocation = gl.getUniformLocation(program, "u_texture");
+	gl.uniform1i(textureLocation, 0);
+
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, quadSpec.drawCount);
 	//requestAnimationFrame(render); 
 }
 var y_offset = 0.1;
-setup_stuff();
+//setup_stuff();
 
 function render()
 {
