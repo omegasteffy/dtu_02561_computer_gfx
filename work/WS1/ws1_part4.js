32427d957074d0142ae5d045105d60c862897703
Steffen Villadsen <omegasteffy@gmail.com>
2018-01-06 20:34:05 +0100

    cleaned up part4

--- work/WS1/ws1_part4.js
+++ work/WS1/ws1_part4.js
@@ -1,78 +1,41 @@
-
-/**
- * Create 
- * @param {any} w
- * @param {any} h
- */
-function quad(gl, w, h) {
-	w = w / 2;
-	h = h / 2;
-	var x = { "type": "quad" };
-	x.drawtype = gl.TRIANGLE_STRIP;
-	x.vertices = new Float32Array([-w, -h, //lower left
-	-w, h
-		, w, -h, //lower right
-		w, h, //upper right
-	]);//upper left
-	x.drawCount = 4;
-	return x;
-}
-var rectSpec;
-var gl;
-var program;
-
-function setup_stuff() {
+//global variables
+let rectSpec;
+let gl;
+let program;
+let theta = 0.5;
+let uniforms;
+function setup_stuff() 
+{
 	console.trace("Started");
 	var canvas = document.getElementById('draw_area');
 
 	gl = WebGLUtils.setupWebGL(canvas);
 
-	 program = initShaders(gl, "vert1", "frag1");
+	program = initShaders(gl, "vert", "frag");
 
 	gl.useProgram(program);
+	uniforms = cacheUniformLocations(gl,program); //instead of requesting all uniform locations we use find these right after init
 	gl.viewport(0.0, 0.0, canvas.width, canvas.height)
 	gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
-	var vertices = new Float32Array(
-		[0.0, 0.0, //middle
-			1.0, 0.0, // right
-			1.0, 1.0,] //top right
-	)
+	const colors = [ CommonColors.red
+					, CommonColors.blue
+					, CommonColors.green
+					, CommonColors.blue];
 
-	var vertice_colors = new Float32Array(
-		[1.0, 0.0, 0.0, 1.0, //Red
-			0.0, 1.0, 0.0, 1.0, //Green
-			0.0, 0.0, 1.0, 1.0, //Blue
-			0.4, 0.1, 0.3, 1.0] //Blue
-	);
-
-	rectSpec = quad(gl, 0.5, 0.5);
-
-	var point_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, point_buffer); // make it the current buffer assigned in WebGL
-	gl.bufferData(gl.ARRAY_BUFFER, rectSpec.vertices, gl.STATIC_DRAW);//link the JS-points and the 
-	var vPos = gl.getAttribLocation(program, "vPosition"); // setup a pointer to match the 
-	gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vPos);
-
-
-	var color_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer); // make it the current buffer assigned in WebGL
-	gl.bufferData(gl.ARRAY_BUFFER, vertice_colors, gl.STATIC_DRAW);//link the JS-points and the 
-	var vColor = gl.getAttribLocation(program, "vColor"); // setup a pointer to match the 
-	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vColor);
-	requestAnimationFrame(render); 
+	rectSpec = rectangle_2d(gl); // this is specified in ../other_common/shapes.js
+	send_floats_to_attribute_buffer("a_Position", rectSpec.vertices, rectSpec.perDrawing, gl, program);
+	send_array_to_attribute_buffer("a_Color", colors, 4, gl, program);
+	requestAnimationFrame(render);
 }
-var theta = 0.5;
-setup_stuff();
 
 function render()
 {
-	var thetaLoc = gl.getUniformLocation(program, "theta");
-	gl.uniform1f(thetaLoc, theta);
+	gl.uniform1f(uniforms.theta, theta); //location is already stored
 	theta += 0.1;
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.drawArrays(rectSpec.drawtype, 0, rectSpec.drawCount);
 	requestAnimationFrame(render); 
 }
+
+setup_stuff(); // start!
