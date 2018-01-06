64bb1399fa71d658c0d4e2d43217aa940a7350ce
Steffen Villadsen <omegasteffy@gmail.com>
2018-01-06 19:55:02 +0100

    cleaned up part3

--- work/WS1/ws1_part3.js
+++ work/WS1/ws1_part3.js
@@ -1,43 +1,26 @@
 console.trace("Started");
-var canvas = document.getElementById('draw_area');
-
-var     gl = WebGLUtils.setupWebGL(canvas);
-
-var program = initShaders(gl,"vert1","frag1");
+const canvas = document.getElementById('draw_area');
+const     gl = WebGLUtils.setupWebGL(canvas);
+const program = initShaders(gl,"vert","frag");
 
 gl.useProgram(program);
 gl.viewport( 0.0 ,0.0, canvas.width,canvas.height)
 gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
-var vertices = new Float32Array(
+const vertices = new Float32Array(
     [0.0,0.0, //middle
     1.0,0.0, // right
     1.0,1.0,] //top right
 )
 
-var vertice_colors = new Float32Array(
+const colors = new Float32Array(
 	[1.0, 0.0, 0.0, 1.0, //Red
 		0.0, 0.0, 1.0, 1.0, //Green
 		0.0, 1.0, 0.0, 1.0] //Blue
 );
 
-
-var point_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, point_buffer); // make it the current buffer assigned in WebGL
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);//link the JS-points and the 
-var vPos = gl.getAttribLocation(program, "vPosition"); // setup a pointer to match the 
-gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(vPos);
-
-
-var color_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer); // make it the current buffer assigned in WebGL
-gl.bufferData(gl.ARRAY_BUFFER, vertice_colors, gl.STATIC_DRAW);//link the JS-points and the 
-var vColor = gl.getAttribLocation(program, "vColor"); // setup a pointer to match the 
-gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(vColor);
-
+send_floats_to_attribute_buffer("a_Position",vertices,2,gl,program);
+send_floats_to_attribute_buffer("a_Color",colors,4,gl,program);
 gl.clear(gl.COLOR_BUFFER_BIT); 
 gl.drawArrays(gl.TRIANGLES,0,3);
-//render();
 console.trace("Ended");
