5a231e4cc41841588d9dcda09c74cf53a9c0d8d9
Steffen Stengaard Villadsen <omegasteffy@gmail.com>
2018-01-08 22:13:12 +0100

    we have moved the camera so it is close to x=0 and y=0, while looking out the z-axis. Furthermore we have extended the z-dimension of the plane

--- work/final_project/final_part1.js
+++ work/final_project/final_part1.js
@@ -1,5 +1,4 @@
 //Globals
-let rectSpec;
 let gl;
 let program;
 let uniforms;
@@ -129,21 +128,23 @@ function setup_stuff()
 	gl = WebGLUtils.setupWebGL(canvas, { alpha: false });
 
 	program = initShaders(gl, "vert", "frag");
+	program_coord = initShaders(gl, "simple_vert", "simple_frag");
 	gl.useProgram(program);
 	uniforms=cacheUniformLocations(gl, program);
+	let uniforms_coord=cacheUniformLocations(gl, program_coord);
 	gl.viewport(0.0, 0.0, canvas.width, canvas.height)
 	gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
-	let eyePos = vec3(); // this is apparently what is meant by default
+	let eyePos = vec3(.5,.3,-0.6); // this is apparently what is meant by default
 	
 	let upVec = vec3(0.0, 1.0, 0.0);//we just need the orientation... it will adjust itself
-	let cameraTarget = vec3(0.0, 0.0, 0.0);// for isometric we should look at origo
+	let cameraTarget = vec3(0.5, 0.3, 0.0);// for isometric we should look at origo
 
 	let cameraMatrix = lookAt(eyePos, cameraTarget, upVec);
 
 	let FieldOfViewY = 45; //deg
 	let AspectRatio = (canvas.width / canvas.height); //should be 1.0
-	let near = 1.0;
+	let near = 0.1;
 	let far = 200.0;
 	let perMatrix = perspective(FieldOfViewY, AspectRatio, near, far);
 
@@ -151,14 +152,22 @@ function setup_stuff()
 //	gl.enable(gl.CULL_FACE); // Ensure the depth of lines and triangles matter, instead of the drawing order... but not required
 
 	trsMatrix = mat4(); // no transformations just yet
+	let coord = coordinateSystem(gl);
+	gl.useProgram(program_coord);
+	gl.uniformMatrix4fv(uniforms_coord.proj_Matrix, false, flatten(perMatrix));
+	gl.uniformMatrix4fv(uniforms_coord.camera_Matrix, false, flatten(cameraMatrix));
+	gl.uniformMatrix4fv(uniforms_coord.trsMatrix, false, flatten(trsMatrix));
+
+	gl.clear(gl.COLOR_BUFFER_BIT );
+	send_array_to_attribute_buffer("a_Position", coord.points, 3, gl, program);
+	gl.drawArrays(gl.LINES, 0, coord.drawCount);
+	gl.useProgram(program);
+	trsMatrix=scalem(1.0,1.0,5);
 	gl.uniformMatrix4fv(uniforms.proj_Matrix, false, flatten(perMatrix));
 	gl.uniformMatrix4fv(uniforms.camera_Matrix, false, flatten(cameraMatrix));
 	gl.uniformMatrix4fv(uniforms.trsMatrix, false, flatten(trsMatrix));
 
-	gl.clear(gl.COLOR_BUFFER_BIT );
-
-	stuff = plane_3d(gl,5,6);
-	rectSpec = rectangle(gl);
+	stuff = plane_3d(gl,50,10);
 	send_floats_to_attribute_buffer("a_Position", stuff.points, 2, gl, program);
 
 	//  //gl.drawArrays(gl.LINE_STRIP, 0,6);
