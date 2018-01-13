047d4a1624715f036654c479a2748f57e857e797
Steffen Villadsen <omegasteffy@gmail.com>
2018-01-13 13:45:37 +0100

    added sphere to the file with shapes

--- work/other_common/shapes.js
+++ work/other_common/shapes.js
@@ -43,3 +43,50 @@ function circle_2d(gl,no_points)
 	x.drawtype = gl.TRIANGLE_FAN;
 	return x;
 }
+function sphere_3d(subDivision)
+{
+	let x = {};
+	x.Points = [];
+	x.Normals = [];
+	x.Colors = [];
+	x.drawCount=0;
+	function tetrahedron(a, b, c, d, n, dest) {
+		divideTriangle(a, b, c, n, dest);
+		divideTriangle(d, c, b, n, dest);
+		divideTriangle(a, d, b, n, dest);
+		divideTriangle(a, c, d, n, dest);
+	}
+
+	function divideTriangle(a, b, c, count, dest) {
+		if (count > 0) {
+			let ab = normalize(mix(a, b, 0.5), true);
+			let ac = normalize(mix(a, c, 0.5), true);
+			let bc = normalize(mix(b, c, 0.5), true);
+			divideTriangle(a, ab, ac, count - 1, dest);
+			divideTriangle(ab, b, bc, count - 1, dest);
+			divideTriangle(bc, c, ac, count - 1, dest);
+			divideTriangle(ab, bc, ac, count - 1, dest);
+		} else {
+			//triangle(a, b, c, dest) ... inlined
+			dest.push(a);
+			dest.push(b);
+			dest.push(c);
+			x.drawCount=x.drawCount+3;
+		}
+	}
+
+
+	let va = vec4(0.0, 0.0, 1.0, 1.0);
+	let vb = vec4(0.0, 0.942809, -0.33, 1.0);
+	let vc = vec4(-0.816497, -0.471405, -0.33, 1.0);
+	let vd = vec4(0.816497, -0.471405, -0.33, 1.0);
+
+	tetrahedron(va, vb, vc, vd, subDivision, x.Points);
+	for (let n = 0; x.Points.length > n; n++) {
+		//color = 0.5*p + 0.3
+		x.Colors[n] = vec4(0.3 + x.Points[n][0], 0.3 + x.Points[n][1], 0.3 + x.Points[n][2], 1.0);
+		x.Normals[n] = x.Points[n];
+	}
+	return x;
+
+}
