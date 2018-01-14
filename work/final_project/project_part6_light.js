5628c9d22190353dfbe95cf3674720a0ff66cff3
Steffen Villadsen <omegasteffy@gmail.com>
2018-01-14 20:32:46 +0100

    Renamed part6 + did a few minor fixes

--- work/final_project/project_part6_light.js
+++ work/final_project/project_part6_light.js
@@ -2,7 +2,7 @@
 let gl;
 let program;
 let uniforms;
-let stuff;
+let grid;
 let time;
 
 setup_stuff();
@@ -14,10 +14,8 @@ function setup_stuff()
 	gl = WebGLUtils.setupWebGL(canvas, { alpha: false });
 
 	program = initShaders(gl, "vert", "frag");
-	program_coord = initShaders(gl, "simple_vert", "simple_frag");
 	gl.useProgram(program);
 	uniforms=cacheUniformLocations(gl, program);
-	let uniforms_coord=cacheUniformLocations(gl, program_coord);
 	gl.viewport(0.0, 0.0, canvas.width, canvas.height)
 	gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
@@ -33,22 +31,14 @@ function setup_stuff()
 	let near = 0.1;
 	let far = 200.0;
 	let perMatrix = perspective(FieldOfViewY, AspectRatio, near, far);
-	let light_pos = vec4(1.5 /*right of cam*/,1.1 /*above*/,-1.7 /*behind*/,0.0); // a bit above, to the right and in the front of the camera
+	let light_pos = vec4(1.5 /*right of cam*/,10.1 /*above*/,-10.7 /*behind*/,0.0); // a bit above, to the right and in the front of the camera
 
 	gl.enable(gl.DEPTH_TEST);
 
 	gl.enable(gl.CULL_FACE); // Ensure the depth of lines and triangles matter, instead of the drawing order... but not required
 
-	trsMatrix = mat4(); // no transformations just yet
-	let coord = coordinateSystem(gl);
-	gl.useProgram(program_coord);
-	gl.uniformMatrix4fv(uniforms_coord.proj_Matrix, false, flatten(perMatrix));
-	gl.uniformMatrix4fv(uniforms_coord.camera_Matrix, false, flatten(cameraMatrix));
-	gl.uniformMatrix4fv(uniforms_coord.trsMatrix, false, flatten(trsMatrix));
-
 	gl.clear(gl.COLOR_BUFFER_BIT );
-	send_array_to_attribute_buffer("a_Position", coord.points, 3, gl, program);
-	gl.drawArrays(gl.LINES, 0, coord.drawCount);
+
 	gl.useProgram(program);
 	trsMatrix=scalem(3.0,1.0,5);
 	gl.uniformMatrix4fv(uniforms.proj_Matrix, false, flatten(perMatrix));
@@ -57,25 +47,16 @@ function setup_stuff()
 	gl.uniform4fv(uniforms.light_pos, flatten(light_pos));
 
 
-	stuff = plane_3d(gl,50,30);
-	send_floats_to_attribute_buffer("a_Position", stuff.points, 2, gl, program);
+	grid = plane_3d(gl,50,30);
+	send_floats_to_attribute_buffer("a_Position", grid.points, 2, gl, program);
 
-	//  //gl.drawArrays(gl.LINE_STRIP, 0,6);
-	// //return;
-	// Create an empty buffer object to store Index buffer
+
 	let index_buffer = gl.createBuffer();
-
-	//// Bind appropriate array buffer to it
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-
-	//// Pass the vertex data to the buffer
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, stuff.indecies, gl.STATIC_DRAW);
-
-	//// Draw the triangle
-	//gl.drawElements(gl.TRIANGLES, 12, gl.UNSIGNED_SHORT, 0);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, grid.indecies, gl.STATIC_DRAW);
 
 	time=0.0;
-render(); // no need for since we only have a static image
+	render();
 
 }
 
@@ -90,6 +71,6 @@ function render()
 	}
 	gl.uniform1f(uniforms.time,time);
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawElements(gl.TRIANGLES, stuff.indecies.length, gl.UNSIGNED_SHORT, 0);
+	gl.drawElements(gl.TRIANGLES, grid.indecies.length, gl.UNSIGNED_SHORT, 0);
 	requestAnimationFrame(render); 
 }
