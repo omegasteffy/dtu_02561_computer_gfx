b26a90fe081bfe2d2be2e2c9d8b60f4a9476065b
Steffen Villadsen <omegasteffy@gmail.com>
2018-01-14 12:16:32 +0100

    clean up and minor fixes

--- work/final_project/final_part1.js
+++ work/final_project/final_part1.js
@@ -14,11 +14,12 @@ function setup_stuff()
 	const canvas = document.getElementById('draw_area');
 	gl = WebGLUtils.setupWebGL(canvas, { alpha: false });
 
+
 	program = initShaders(gl, "vert", "frag");
 	program_coord = initShaders(gl, "simple_vert", "simple_frag");
-	gl.useProgram(program);
-	uniforms=cacheUniformLocations(gl, program);
-	let uniforms_coord=cacheUniformLocations(gl, program_coord);
+
+	uniforms = cacheUniformLocations(gl, program);
+	let uniforms_coord = cacheUniformLocations(gl, program_coord);
 	gl.viewport(0.0, 0.0, canvas.width, canvas.height)
 	gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
@@ -36,54 +37,38 @@ function setup_stuff()
 	let perMatrix = perspective(FieldOfViewY, AspectRatio, near, far);
 
 //	gl.enable(gl.DEPTH_TEST);
-//	gl.enable(gl.CULL_FACE); // Ensure the depth of lines and triangles matter, instead of the drawing order... but not required
+	gl.enable(gl.CULL_FACE);
 
-	trsMatrix = mat4(); // no transformations just yet
-	let coord = coordinateSystem(gl);
+	gl.clear(gl.COLOR_BUFFER_BIT );
+
+	//Draw the coordinate system
 	gl.useProgram(program_coord);
+	trsMatrix = mat4(); // no for the coordinate system just yet
+	let coord = coordinateSystem(gl);
 	gl.uniformMatrix4fv(uniforms_coord.proj_Matrix, false, flatten(perMatrix));
 	gl.uniformMatrix4fv(uniforms_coord.camera_Matrix, false, flatten(cameraMatrix));
 	gl.uniformMatrix4fv(uniforms_coord.trsMatrix, false, flatten(trsMatrix));
 
-	gl.clear(gl.COLOR_BUFFER_BIT );
-	send_array_to_attribute_buffer("a_Position", coord.points, 3, gl, program);
+	send_array_to_attribute_buffer("a_Position", coord.points, 3, gl, program_coord);
+	send_array_to_attribute_buffer("a_Color", coord.colors, 4, gl, program_coord);
 	gl.drawArrays(gl.LINES, 0, coord.drawCount);
+
+	//Draw the grid
 	gl.useProgram(program);
-	trsMatrix=scalem(1.0,1.0,5);
+	trsMatrix=scalem( 1.0 , 1.0 , 5.0 ); //extend 5x as long in the z-direction
 	gl.uniformMatrix4fv(uniforms.proj_Matrix, false, flatten(perMatrix));
 	gl.uniformMatrix4fv(uniforms.camera_Matrix, false, flatten(cameraMatrix));
 	gl.uniformMatrix4fv(uniforms.trsMatrix, false, flatten(trsMatrix));
 
-	stuff = plane_3d(gl,50,10);
-	send_floats_to_attribute_buffer("a_Position", stuff.points, 2, gl, program);
+	grid = plane_3d(gl,50,10);
+	send_floats_to_attribute_buffer("a_Position", grid.points, 2, gl, program);
+	
+	let index_buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, grid.line_indecies, gl.STATIC_DRAW);
 
-	//  //gl.drawArrays(gl.LINE_STRIP, 0,6);
-	// //return;
-	// // Create an empty buffer object to store Index buffer
-	// let index_buffer = gl.createBuffer();
-
-	// //// Bind appropriate array buffer to it
-	// gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-
-	// //// Pass the vertex data to the buffer
-	// gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, stuff.indecies, gl.STATIC_DRAW);
-
-	// //// Draw the triangle
-	// gl.drawElements(gl.TRIANGLES, 12, gl.UNSIGNED_SHORT, 0);
-
-	send_floats_to_attribute_buffer("a_Position", stuff.points, 2, gl, program);
-	let index_buffer2 = gl.createBuffer();
-
-	//// Bind appropriate array buffer to it
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer2);
-
-	//// Pass the vertex data to the buffer
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, stuff.line_indecies, gl.STATIC_DRAW);
-
-	//// Draw the triangle
-	gl.drawElements(gl.LINES, stuff.line_indecies.length, gl.UNSIGNED_SHORT, 0);
-
-//	gl.drawArrays(gl.TRIANGLE_STRIP, 0, rectSpec.drawCount);
+	//// Draw the triangles
+	gl.drawElements(gl.LINES, grid.line_indecies.length, gl.UNSIGNED_SHORT, 0);
 //	render(); // no need for since we only have a static image
 }
 
@@ -93,6 +78,6 @@ function render()
 
 	gl.clear(gl.COLOR_BUFFER_BIT);
 //	gl.drawArrays(gl.TRIANGLE_STRIP, 0, rectSpec.drawCount);
-gl.drawElements(gl.TRIANGLES, stuff.indecies.length/3, gl.UNSIGNED_SHORT, 0);
+ 	gl.drawElements(gl.TRIANGLES, grid.indecies.length/3, gl.UNSIGNED_SHORT, 0);
 	requestAnimationFrame(render); 
 }
